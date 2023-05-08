66a4f4eec108557746649754bfaa8513684f3f50
umamahesh124 <132579658+umamahesh124@users.noreply.github.com>
2023-05-08 20:26:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,5 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.text("The fruit list contains:")
-streamlit.text(my_data_row.json())
+streamlit.text(pandas.json_normalize(my_data_row.json()))
 
