037d157a8f5f06d92ac7e885d6d906dc185fa5e6
umamahesh124 <132579658+umamahesh124@users.noreply.github.com>
2023-05-08 21:07:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,5 +49,5 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.header("The fruit list contains:")
 #fruitlist_normalized=pandas.json_normalize(my_data_row.json())
-streamlit.text(my_data_row)
+streamlit.text(my_data_row.json())
 
