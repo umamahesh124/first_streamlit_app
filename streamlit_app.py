8d1681c03f0a776c89e31aa96d87f5f7dfca288e
umamahesh124 <132579658+umamahesh124@users.noreply.github.com>
2023-05-08 21:30:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,3 +54,5 @@ streamlit.dataframe(fruitlist_normalized)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding', add_my_fruit)
+
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from strimlit')")
