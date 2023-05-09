83b19ae4d4da2e4415bc04fede38006df8181ae8
umamahesh124 <132579658+umamahesh124@users.noreply.github.com>
2023-05-09 11:05:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,8 +55,8 @@ try:
 #streamlit.text(fruityvice_response.json())
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #streamlit.dataframe(fruityvice_normalized)
-#except URLError as e:
-#   streamlit.error()
+except URLError as e:
+   streamlit.error()
 
 #streamlit.stop()
 #import snowflake.connector
