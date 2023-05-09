57e0198bec185842fdbe374bc09b45e0bf8f3561
umamahesh124 <132579658+umamahesh124@users.noreply.github.com>
2023-05-09 12:17:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,7 @@ streamlit.write('Thanks for adding', add_my_fruit)
 
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-         my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values (' "kiwi + jackfruit + guava + papaya" ')")
+         my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('kiwi')")
          return "Thanks for adding" + new_fruit   
 
 
